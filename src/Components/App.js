f90d6ba2a819d4a922f1ddcb65abee43393afb27
Teds <marktedra29@gmail.com>
2025-06-14 20:35:52 +0800

    done

--- src/Components/App.js
+++ src/Components/App.js
@@ -100,7 +100,7 @@ export default function App() {
   );
 
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    fetch("/question.json")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
