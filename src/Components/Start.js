b0d860b2d95be2d44f1cf41647ef6a2d7c10bdee
Teds <marktedra29@gmail.com>
2025-06-26 08:25:24 +0800

    do a thing
    done update

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -1,8 +1,8 @@
 function Start({ numQuestions, dispatch }) {
   return (
     <div className="start">
-      <h2>Welcome to The React Quiz!</h2>
-      <h3>{numQuestions} questions to test your React mastery</h3>
+      <h2>Welcome to The Tech Quiz!</h2>
+      <h3>{numQuestions} questions to test your Brain</h3>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "start" })}
